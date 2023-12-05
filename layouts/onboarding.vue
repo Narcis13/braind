d57e3d4e443dc31e097c584778edaf3ba2eff5d0
Narcis13 <Narcis75@gmail.com>
2023-12-05 20:06:34 +0200

    am inceput

---
+++ layouts/onboarding.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex justify-center q-pa-xl">
+        <slot />
+    </div>
+</template>
---
+++ pages/inregistrare/client.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+definePageMeta({
+    layout:'onboarding'
+})
+
+let numeintreg = ref("")
+let email = ref("")
+let telefon = ref("")
+let parola = ref("")
+let confirmareparola = ref("")
+
+let eDeAcord = ref(false)
+let eAdministrator = ref(false)
+</script>
+
+<template>
+    <q-card class="my-card" flat bordered>
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+
+      <q-card-section>
+        <div class="text-overline text-orange-9">brAInd.ro</div>
+        <div class="text-h5 q-mt-sm q-mb-xs">Inregistrare utilizator</div>
+        <q-input standout="bg-indigo text-white" v-model="numeintreg" label="Nume complet" />
+        <q-input standout="bg-indigo text-white" v-model="email" label="Adresa email" />
+        <q-input standout="bg-indigo text-white" v-model="parola" label="Parola" />
+        <q-input standout="bg-indigo text-white" v-model="confirmareparola" label="Confirmare parola" />
+        <q-input standout="bg-indigo text-white" v-model="telefon" label="Telefon mobil" />
+      </q-card-section>
+
+      <q-card-section>
+        <q-checkbox v-model="eAdministrator" label="Confirm ca am calitatea de administrator si autoritatea de a semna contracte in numele firmei" />
+        <q-checkbox v-model="eDeAcord" label="Sunt de acord cu termenii si conditiile" />
+      </q-card-section>
+
+      <q-card-actions>
+        <q-btn flat color="primary" label="Trimite" />
+        
+      </q-card-actions>
+
+    </q-card>
+</template>
+<style >
+.my-card{
+    width: 100%;
+    max-width: 350px;
+}
+
+</style>
