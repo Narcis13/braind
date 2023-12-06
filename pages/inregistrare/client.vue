49d5bf8d09bc84b75e3ce60ce29110c96f58768f
narcis13 <narcis75@gmail.com>
2023-12-06 14:20:42 +0200

    hai

--- pages/inregistrare/client.vue
+++ pages/inregistrare/client.vue
@@ -12,6 +12,19 @@ let confirmareparola = ref("")
 
 let eDeAcord = ref(false)
 let eAdministrator = ref(false)
+
+
+let mailRules= [value=>{
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)||'Email invalid!'
+}]
+//^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])[A-Za-z\d!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]{8,}$
+let passRules= [value=>{
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])[A-Za-z\d!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]{8,}$/.test(value)||'Parola insuficient de complexa !'
+}]
+let confRules= [value=>{
+  return value===parola.value||'Parola introdusa difera de cea de mai sus!'
+}]
+
 </script>
 
 <template>
@@ -23,10 +36,10 @@ let eAdministrator = ref(false)
       <q-card-section>
         <div class="text-overline text-orange-9">brAInd.ro</div>
         <div class="text-h5 q-mt-sm q-mb-xs">Inregistrare utilizator</div>
-        <q-input standout="bg-indigo text-white" v-model="numeintreg" label="Nume complet" />
-        <q-input standout="bg-indigo text-white" v-model="email" label="Adresa email" />
-        <q-input standout="bg-indigo text-white" v-model="parola" label="Parola" />
-        <q-input standout="bg-indigo text-white" v-model="confirmareparola" label="Confirmare parola" />
+        <q-input standout="bg-indigo text-white" v-model="numeintreg" label="Nume complet" :rules="[val => !!val || 'Camp obligatoriu',val => (val && val.length > 4) || 'Min. 5 caractere']"/>
+        <q-input standout="bg-indigo text-white"  bottom-slots  v-model="email"  lazy-rules :rules="mailRules" label="Adresa email" />
+        <q-input hint="Minim 8 caractere, litere mari, mici, cifre si semne de punctuatie" lazy-rules :rules="passRules" type="password" standout="bg-indigo text-white" v-model="parola" label="Parola" />
+        <q-input class="q-mt-md" type="password" standout="bg-indigo text-white" v-model="confirmareparola" lazy-rules :rules="confRules" label="Confirmare parola" />
         <q-input standout="bg-indigo text-white" v-model="telefon" label="Telefon mobil" />
       </q-card-section>
 
@@ -37,7 +50,9 @@ let eAdministrator = ref(false)
 
       <q-card-actions>
         <q-btn flat color="primary" label="Trimite" />
-        
+       
+        <q-space />
+        <q-btn flat color="secondary" label="Termeni si Conditii" />
       </q-card-actions>
 
     </q-card>
