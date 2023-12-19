4103aaa1a511990c8bef83c19ddd1033ae9ee938
narcis13 <narcis75@gmail.com>
2023-12-19 13:44:05 +0200

    cacat

--- server/api/firme/asignateclientului/[cid].js
+++ server/api/firme/asignateclientului/[cid].js
@@ -1,4 +1,12 @@
-export default defineEventHandler( (event)=>{
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+export default defineEventHandler( async (event)=>{
     const {cid} = event.context.params
+    const firme =await prisma.Firme.findMany();
+    
+    console.log(firme)
     return {firma:cid}
 })
