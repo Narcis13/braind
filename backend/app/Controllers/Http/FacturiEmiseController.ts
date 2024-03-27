fe263a37fe8d22d857224a9920321aa0ba046c37
narcis13 <narcis75@gmail.com>
2024-03-27 13:47:47 +0200

    un pic

--- backend/app/Controllers/Http/FacturiEmiseController.ts
+++ backend/app/Controllers/Http/FacturiEmiseController.ts
@@ -4,8 +4,18 @@ import Database from '@ioc:Adonis/Lucid/Database'
 export default class FacturiEmiseController {
 
     public async printfactura({view,params}){
-       let lista = await  Database.rawQuery('select * from cdata.Firme')
-        console.log(lista[0]) 
+
+       let sql=`
+       SELECT fe.id id,furnizor.denumire denumirefurnizor,furnizor.cui codfiscalfurnizor,cl.denumire numeclient,cl.codfiscal codfiscalclient, fe.dataFactura dataFactura,fe.scadenta scadenta,fe.serie serie, fe.numar numar ,fe.intocmit intocmit,fe.cnp cnp, fe.mentiuni mentiuni,produse.denumire numeprodus,liniife.descriere descriere,liniife.um um, liniife.cantitate cantitate,liniife.pret pret, liniife.valoare valoare FROM cdata.facturi_emise fe
+       inner join cdata.clienti cl on cl.id=fe.idClient
+       inner join cdata.Firme furnizor on furnizor.id=fe.id_client
+       inner join cdata.linii_facturi_emise liniife on liniife.idFacturaEmisa=fe.id
+       inner join cdata.produsi produse on produse.id=liniife.idProdus
+            where fe.id=${params.idf};
+       `;
+
+       let lista = await  Database.rawQuery(sql)
+        console.log(params.idf,lista[0]) 
         return view.render('femise/model1')
     }
 
