54eca49848fcdbe2826f9acadd49ff288f76c54b
Narcis13 <Narcis75@gmail.com>
2024-03-27 20:07:15 +0200

    inceput nou

--- backend/app/Controllers/Http/FacturiEmiseController.ts
+++ backend/app/Controllers/Http/FacturiEmiseController.ts
@@ -6,7 +6,7 @@ export default class FacturiEmiseController {
     public async printfactura({view,params}){
 
        let sql=`
-       SELECT fe.id id,furnizor.denumire denumirefurnizor,furnizor.cui codfiscalfurnizor,cl.denumire numeclient,cl.codfiscal codfiscalclient, fe.dataFactura dataFactura,fe.scadenta scadenta,fe.serie serie, fe.numar numar ,fe.intocmit intocmit,fe.cnp cnp, fe.mentiuni mentiuni,produse.denumire numeprodus,liniife.descriere descriere,liniife.um um, liniife.cantitate cantitate,liniife.pret pret, liniife.valoare valoare FROM cdata.facturi_emise fe
+       SELECT fe.id id,furnizor.denumire denumirefurnizor,furnizor.cui codfiscalfurnizor,cl.denumire numeclient,cl.codfiscal codfiscalclient, DATE_FORMAT(fe.dataFactura, '%d.%m.%Y') AS dataFactura,fe.scadenta scadenta,fe.serie serie, fe.numar numar ,fe.intocmit intocmit,fe.cnp cnp, fe.mentiuni mentiuni,produse.denumire numeprodus,liniife.descriere descriere,liniife.um um, liniife.cantitate cantitate,liniife.pret pret, liniife.valoare valoare FROM cdata.facturi_emise fe
        inner join cdata.clienti cl on cl.id=fe.idClient
        inner join cdata.Firme furnizor on furnizor.id=fe.id_client
        inner join cdata.linii_facturi_emise liniife on liniife.idFacturaEmisa=fe.id
@@ -16,7 +16,7 @@ export default class FacturiEmiseController {
 
        let lista = await  Database.rawQuery(sql)
         console.log(params.idf,lista[0]) 
-        return view.render('femise/model1')
+        return view.render('femise/model1',{factura:lista[0]})
     }
 
 }
