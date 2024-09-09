06f4a8756d4ab19a30e42218b59480a2e63957ab
Narcis13 <Narcis75@gmail.com>
2024-09-09 19:05:57 +0300

    gata iban diferit

--- backend/app/Controllers/Http/FacturiEmiseController.ts
+++ backend/app/Controllers/Http/FacturiEmiseController.ts
@@ -15,7 +15,7 @@ export default class FacturiEmiseController {
     public async printfactura({view,params}){
 
        let sql=`
-       SELECT fe.id id,furnizor.denumire denumirefurnizor,furnizor.alias alias,furnizor.cui codfiscalfurnizor,furnizor.banca banca,furnizor.iban iban,cl.denumire numeclient,cl.codfiscal codfiscalclient, DATE_FORMAT(fe.dataFactura, '%d.%m.%Y') AS dataFactura,fe.scadenta scadenta,fe.serie serie, fe.numar numar ,fe.intocmit intocmit,fe.cnp cnp, fe.mentiuni mentiuni,produse.denumire numeprodus,liniife.descriere descriere,liniife.um um, liniife.cantitate cantitate,liniife.pret pret, liniife.valoare valoare FROM cdata.facturi_emise fe
+       SELECT fe.id id,furnizor.denumire denumirefurnizor,furnizor.alias alias,furnizor.cui codfiscalfurnizor,furnizor.banca banca,produse.iban iban,cl.denumire numeclient,cl.codfiscal codfiscalclient, DATE_FORMAT(fe.dataFactura, '%d.%m.%Y') AS dataFactura,fe.scadenta scadenta,fe.serie serie, fe.numar numar ,fe.intocmit intocmit,fe.cnp cnp, fe.mentiuni mentiuni,produse.denumire numeprodus,liniife.descriere descriere,liniife.um um, liniife.cantitate cantitate,liniife.pret pret, liniife.valoare valoare FROM cdata.facturi_emise fe
        inner join cdata.clienti cl on cl.id=fe.idClient
        inner join cdata.Firme furnizor on furnizor.id=fe.id_client
        inner join cdata.linii_facturi_emise liniife on liniife.idFacturaEmisa=fe.id
